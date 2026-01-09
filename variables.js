55cad91c9e08bf306768fe62aed7d7f69888609a
duarjc2019 <dua24004@byui.edu>
2026-01-09 08:46:53 -0700

    Initial Commit

---
+++ variables.js
@@ -0,0 +1,19 @@
+//Constants and Variables
+
+const PI = 3.14;
+let radius = 3;
+area = radius * radius * PI;
+
+console.log(area)
+
+radus = 1256;
+
+console.log(radus)
+
+//Type Coersion
+
+const one = 1;
+const two = '2';
+area = one + two;
+
+console.log(area)

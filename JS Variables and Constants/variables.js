47534c99e1e03ceede1650ddb697dfa3d964faab
duarjc2019 <dua24004@byui.edu>
2026-01-09 22:31:01 -0700

    Added code to match output in Developer Tools

--- JS Variables and Constants/variables.js
+++ JS Variables and Constants/variables.js
@@ -17,3 +17,26 @@ const two = '2';
 area = one + two;
 
 console.log(area)
+
+const one1 = 1;
+area = one1 + one1;
+
+console.log(area)
+
+const two2 = 2;
+area = one1 + two2;
+
+console.log(area)
+
+//Global and Block Scope
+
+
+let course = "CSE131"; //global scope
+if (true) {
+    let student = "John";
+    console.log(course);  //works just fine, course is global
+    console.log(student); //works just fine, it's being accessed within the block
+}
+console.log(course); //works fine, course is global
+console.log(student); //does not work, can't access a block variable outside the block
+                    

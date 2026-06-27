4104fa180cb3d589f151e02ea044081cc72f3c3b
duarjc79 <dua24004@byui.edu>
2026-06-27 14:47:09 -0600

    created hikes html and js files with code

---
+++ week10/sortfilter.js
@@ -0,0 +1,86 @@
+
+const simpleList = ["oranges", "grapes", "lemons", "apples", "Bananas", "watermelons", "coconuts", "broccoli", "mango"];
+
+let simpleSort = simpleList.sort();
+
+let lowerList = simpleList.map(function(fruit) {
+    return fruit.toLowerCase();
+})
+
+let lowerSort = lowerList.sort();
+
+let searchTerm = 'an';
+
+let filterFruit = lowerSort.filter(searchFruit);
+
+function searchFruit(item){
+    return item.includes(searchTerm);
+}
+
+function compareFn(a,b) {
+  if (a.price < b.price) {
+    return -1;
+  } else if (a.price > b.price) {
+    return 1;
+  }
+ return 0;
+}
+                
+const products = [
+  {
+    productName: "Wireless Mouse",
+    price: 29.99
+  },
+  {
+    productName: "Bluetooth Keyboard",
+    price: 49.99
+  },
+  {
+    productName: "Laptop Stand",
+    price: 39.99
+  }
+];
+
+let productSort = products.sort(compareFn);
+
+
+const animals = [
+  {
+    name: "Lion",
+    traits: ["brave", "strong", "fierce", "wild"]
+  },
+  {
+    name: "Elephant",
+    traits: ["large", "gentle", "smart", "wild"]
+  },
+  {
+    name: "Fox",
+    traits: ["sly", "quick", "clever", "wild"]
+  },
+  {
+    name: "Dog",
+    traits: ["loyal", "friendly", "playful", "cuddly"]
+  },
+  {
+    name: "Cat",
+    traits: ["quiet", "independent", "curious", "cuddly"]
+  }
+];
+
+let query = ''
+
+let filteredList = animals.filter(searchList);
+
+function searchList(item){
+    return item.name.toLowerCase().includes(query.toLowerCase());
+}
+
+let queryTrait = 'wild';
+
+let filteredTraits = animals.filter(searchTraits);
+
+function searchTraits(item){
+    return item.traits.find((trait) => trait.toLowerCase().includes(queryTrait.toLowerCase()));
+}
+
+console.log(filteredTraits);
